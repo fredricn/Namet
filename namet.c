93708a1ef15e7006c68f587f31d7f3fc05bff64e
Fredric N <fredric@fiktivkod.org>
2011-09-20 10:08:10 +0200

    adding path support

--- namet.c
+++ namet.c
@@ -65,40 +65,61 @@ void flip(char *str) {
     }
 }
 
+char *bname(const char *path) {
+    
+    char *file;
+    
+    file = strrchr(path, '/');
+    
+    if (file == NULL) 
+        file = (char *)path;
+    else 
+        ++file;
+
+    return file;
+}
+
 int main (const int argc, const char **argv) {
 
-    const char *oldname = argv[2];
-    char *name = malloc(sizeof(char) * (strlen(oldname)) + 1);
+    const char *pathOld = argv[2];
+    char *pathNew = malloc(sizeof(char) * (strlen(pathOld)) + 1);
+    char *nameOld = bname(pathOld);
+    char *nameNew;
     struct stat st;
+    
+    strcpy(pathNew,pathOld);
 
-    strcpy(name,oldname);
+    nameNew = bname(pathNew);
 
     if (argc != 3) {
         usage();
         return 1;
     }
     
-    /* find option */
-    if (strcmp(LOWER,argv[1]) == 0) {
-        strtolower(name);
-    } else if (strcmp(UPPER,argv[1]) == 0) {
-        strtoupper(name);
-    } else if (strcmp(DYNAMIC, argv[1]) == 0) {
-        dynamic_transform(name);
-    } else if (strcmp(FLIP, argv[1]) == 0) {
-        flip(name);
-    }
-   
-    if ((stat(oldname, &st)) == -1) {
+    /* file exists? */
+    if ((stat(pathOld, &st)) == -1) {
         printf("The file does not exist!\n");
         return -1;
     }
+    
+    /* find option */
+    if (strcmp(LOWER,argv[1]) == 0) {
+        strtolower(nameNew);
+    } else if (strcmp(UPPER,argv[1]) == 0) {
+        strtoupper(nameNew);
+    } else if (strcmp(DYNAMIC, argv[1]) == 0) {
+        dynamic_transform(nameNew);
+    } else if (strcmp(FLIP, argv[1]) == 0) {
+        flip(nameNew);
+    } 
 
-    rename(oldname, name);
-    printf("Changed name from %s to %s\n", oldname, name);
-
-    if (name != NULL)
-        free(name);
+    /* rename file */
+    rename(pathOld, pathNew);
+    printf("Changed name from %s to %s\n", nameOld, nameNew);
+    
+    /* cleanup */
+    if (pathNew != NULL)
+        free(pathNew);
 
     return 0;
 }
